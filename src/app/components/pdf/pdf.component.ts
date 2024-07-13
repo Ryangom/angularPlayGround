f29d579aed1969ce07bebd601dc6c38b4bedab6a
Jonas Ryan Gomes <33547792+Ryangom@users.noreply.github.com>
2024-07-13 18:35:07 +0600

    sdfsdfsdf

--- src/app/components/pdf/pdf.component.ts
+++ src/app/components/pdf/pdf.component.ts
@@ -9,8 +9,436 @@ import { PdfService } from 'src/app/service/pdf.service';
   styleUrls: ['./pdf.component.css']
 })
 export class PdfComponent {
-
-
+  data: any = {
+    status: "OK",
+    payload: {
+      semesterName: "Second Year First Semester",
+      semesterCredit: 19,
+      departmentName: "Department of Information and Communication Technology",
+      batchName: "ICT-2022",
+      examSession: "2021-2022",
+      session: "2021-2022",
+      tabulationSheetGenerate: false,
+      students: [
+        {
+          studentUniqueId: "12109050",
+          studentName: "Re-Add",
+          totalCredit: 0,
+          semesterGpa: "0.00",
+          remarks: "Submitted",
+          subjects: [
+            {
+              subjectCode: "MATH-301",
+              subjectName: "Differential Equation",
+              subjectCredit: 3,
+              beforeFinalMarks: 38,
+              finalMarks: "0.00",
+              totalMarks: "Submitted",
+              letterGrade: "Submitted",
+              gpa: "Submitted"
+            },
+            {
+              subjectCode: "ICT-303",
+              subjectName: "Object Oriented Programming ",
+              subjectCredit: 3,
+              beforeFinalMarks: 32,
+              finalMarks: "0.00",
+              totalMarks: "Submitted",
+              letterGrade: "Submitted",
+              gpa: "Submitted"
+            },
+            {
+              subjectCode: "ICT-304",
+              subjectName: "Object Oriented Programming Sessional",
+              subjectCredit: 1,
+              beforeFinalMarks: 30,
+              finalMarks: "0.00",
+              totalMarks: "Submitted",
+              letterGrade: "Submitted",
+              gpa: "Submitted"
+            },
+            {
+              subjectCode: "ECO-305",
+              subjectName: "Economics",
+              subjectCredit: 2,
+              beforeFinalMarks: 16,
+              finalMarks: "0.00",
+              totalMarks: "Submitted",
+              letterGrade: "Submitted",
+              gpa: "Submitted"
+            },
+            {
+              subjectCode: "ICT-307",
+              subjectName: "Algorithm Design",
+              subjectCredit: 3,
+              beforeFinalMarks: 37,
+              finalMarks: "0.00",
+              totalMarks: "Submitted",
+              letterGrade: "Submitted",
+              gpa: "Submitted"
+            },
+            {
+              subjectCode: "ICT-308",
+              subjectName: "Algorithm Design Sessional",
+              subjectCredit: 1,
+              beforeFinalMarks: 35,
+              finalMarks: "0.00",
+              totalMarks: "Submitted",
+              letterGrade: "Submitted",
+              gpa: "Submitted"
+            },
+            {
+              subjectCode: "ICT-309",
+              subjectName: "Numerical Analysis",
+              subjectCredit: 3,
+              beforeFinalMarks: 39,
+              finalMarks: "0.00",
+              totalMarks: "Submitted",
+              letterGrade: "Submitted",
+              gpa: "Submitted"
+            },
+            {
+              subjectCode: "ICT-310",
+              subjectName: "Numerical Analysis Sessional",
+              subjectCredit: 1,
+              beforeFinalMarks: 38,
+              finalMarks: "0.00",
+              totalMarks: "Submitted",
+              letterGrade: "Submitted",
+              gpa: "Submitted"
+            },
+            {
+              subjectCode: "ACC-311",
+              subjectName: "Financial and Managerial Accounting",
+              subjectCredit: 2,
+              beforeFinalMarks: 17,
+              finalMarks: "0.00",
+              totalMarks: "Submitted",
+              letterGrade: "Submitted",
+              gpa: "Submitted"
+            }
+          ],
+          viva: {},
+          thesis: {},
+          project: {},
+          internship: {},
+          industryAttachment: {}
+        },
+        {
+          studentUniqueId: "12209002",
+          studentName: "SAUDHA HUQUE SHOILEE",
+          totalCredit: 0,
+          semesterGpa: "0.00",
+          remarks: "Submitted",
+          subjects: [
+            {
+              subjectCode: "MATH-301",
+              subjectName: "Differential Equation",
+              subjectCredit: 3,
+              beforeFinalMarks: 29,
+              finalMarks: "0.00",
+              totalMarks: "Submitted",
+              letterGrade: "Submitted",
+              gpa: "Submitted"
+            },
+            {
+              subjectCode: "ICT-303",
+              subjectName: "Object Oriented Programming ",
+              subjectCredit: 3,
+              beforeFinalMarks: 29,
+              finalMarks: "0.00",
+              totalMarks: "Submitted",
+              letterGrade: "Submitted",
+              gpa: "Submitted"
+            },
+            {
+              subjectCode: "ICT-304",
+              subjectName: "Object Oriented Programming Sessional",
+              subjectCredit: 1,
+              beforeFinalMarks: 29,
+              finalMarks: "0.00",
+              totalMarks: "Submitted",
+              letterGrade: "Submitted",
+              gpa: "Submitted"
+            },
+            {
+              subjectCode: "ECO-305",
+              subjectName: "Economics",
+              subjectCredit: 2,
+              beforeFinalMarks: 14,
+              finalMarks: "0.00",
+              totalMarks: "Submitted",
+              letterGrade: "Submitted",
+              gpa: "Submitted"
+            },
+            {
+              subjectCode: "ICT-307",
+              subjectName: "Algorithm Design",
+              subjectCredit: 3,
+              beforeFinalMarks: 32,
+              finalMarks: "0.00",
+              totalMarks: "Submitted",
+              letterGrade: "Submitted",
+              gpa: "Submitted"
+            },
+            {
+              subjectCode: "ICT-308",
+              subjectName: "Algorithm Design Sessional",
+              subjectCredit: 1,
+              beforeFinalMarks: 32,
+              finalMarks: "0.00",
+              totalMarks: "Submitted",
+              letterGrade: "Submitted",
+              gpa: "Submitted"
+            },
+            {
+              subjectCode: "ICT-309",
+              subjectName: "Numerical Analysis",
+              subjectCredit: 3,
+              beforeFinalMarks: 32.5,
+              finalMarks: "0.00",
+              totalMarks: "Submitted",
+              letterGrade: "Submitted",
+              gpa: "Submitted"
+            },
+            {
+              subjectCode: "ICT-310",
+              subjectName: "Numerical Analysis Sessional",
+              subjectCredit: 1,
+              beforeFinalMarks: 32,
+              finalMarks: "0.00",
+              totalMarks: "Submitted",
+              letterGrade: "Submitted",
+              gpa: "Submitted"
+            },
+            {
+              subjectCode: "ACC-311",
+              subjectName: "Financial and Managerial Accounting",
+              subjectCredit: 2,
+              beforeFinalMarks: 17,
+              finalMarks: "0.00",
+              totalMarks: "Submitted",
+              letterGrade: "Submitted",
+              gpa: "Submitted"
+            }
+          ],
+          viva: {},
+          thesis: {},
+          project: {},
+          internship: {},
+          industryAttachment: {}
+        },
+        {
+          studentUniqueId: "12209003",
+          studentName: "SUPARNA DEBNATH TONNI",
+          totalCredit: 0,
+          semesterGpa: "0.00",
+          remarks: "Submitted",
+          subjects: [
+            {
+              subjectCode: "MATH-301",
+              subjectName: "Differential Equation",
+              subjectCredit: 3,
+              beforeFinalMarks: 27,
+              finalMarks: "0.00",
+              totalMarks: "Submitted",
+              letterGrade: "Submitted",
+              gpa: "Submitted"
+            },
+            {
+              subjectCode: "ICT-303",
+              subjectName: "Object Oriented Programming ",
+              subjectCredit: 3,
+              beforeFinalMarks: 28,
+              finalMarks: "0.00",
+              totalMarks: "Submitted",
+              letterGrade: "Submitted",
+              gpa: "Submitted"
+            },
+            {
+              subjectCode: "ICT-304",
+              subjectName: "Object Oriented Programming Sessional",
+              subjectCredit: 1,
+              beforeFinalMarks: 22,
+              finalMarks: "0.00",
+              totalMarks: "Submitted",
+              letterGrade: "Submitted",
+              gpa: "Submitted"
+            },
+            {
+              subjectCode: "ECO-305",
+              subjectName: "Economics",
+              subjectCredit: 2,
+              beforeFinalMarks: 16,
+              finalMarks: "0.00",
+              totalMarks: "Submitted",
+              letterGrade: "Submitted",
+              gpa: "Submitted"
+            },
+            {
+              subjectCode: "ICT-307",
+              subjectName: "Algorithm Design",
+              subjectCredit: 3,
+              beforeFinalMarks: 30,
+              finalMarks: "0.00",
+              totalMarks: "Submitted",
+              letterGrade: "Submitted",
+              gpa: "Submitted"
+            },
+            {
+              subjectCode: "ICT-308",
+              subjectName: "Algorithm Design Sessional",
+              subjectCredit: 1,
+              beforeFinalMarks: 28,
+              finalMarks: "0.00",
+              totalMarks: "Submitted",
+              letterGrade: "Submitted",
+              gpa: "Submitted"
+            },
+            {
+              subjectCode: "ICT-309",
+              subjectName: "Numerical Analysis",
+              subjectCredit: 3,
+              beforeFinalMarks: 26.5,
+              finalMarks: "0.00",
+              totalMarks: "Submitted",
+              letterGrade: "Submitted",
+              gpa: "Submitted"
+            },
+            {
+              subjectCode: "ICT-310",
+              subjectName: "Numerical Analysis Sessional",
+              subjectCredit: 1,
+              beforeFinalMarks: 25,
+              finalMarks: "0.00",
+              totalMarks: "Submitted",
+              letterGrade: "Submitted",
+              gpa: "Submitted"
+            },
+            {
+              subjectCode: "ACC-311",
+              subjectName: "Financial and Managerial Accounting",
+              subjectCredit: 2,
+              beforeFinalMarks: 15,
+              finalMarks: "0.00",
+              totalMarks: "Submitted",
+              letterGrade: "Submitted",
+              gpa: "Submitted"
+            }
+          ],
+          viva: {},
+          thesis: {},
+          project: {},
+          internship: {},
+          industryAttachment: {}
+        },
+        {
+          studentUniqueId: "12209004",
+          studentName: "MD. DIPTO RAHMAN",
+          totalCredit: 0,
+          semesterGpa: "0.00",
+          remarks: "Submitted",
+          subjects: [
+            {
+              subjectCode: "MATH-301",
+              subjectName: "Differential Equation",
+              subjectCredit: 3,
+              beforeFinalMarks: 29,
+              finalMarks: "0.00",
+              totalMarks: "Submitted",
+              letterGrade: "Submitted",
+              gpa: "Submitted"
+            },
+            {
+              subjectCode: "ICT-303",
+              subjectName: "Object Oriented Programming ",
+              subjectCredit: 3,
+              beforeFinalMarks: 30,
+              finalMarks: "0.00",
+              totalMarks: "Submitted",
+              letterGrade: "Submitted",
+              gpa: "Submitted"
+            },
+            {
+              subjectCode: "ICT-304",
+              subjectName: "Object Oriented Programming Sessional",
+              subjectCredit: 1,
+              beforeFinalMarks: 26,
+              finalMarks: "0.00",
+              totalMarks: "Submitted",
+              letterGrade: "Submitted",
+              gpa: "Submitted"
+            },
+            {
+              subjectCode: "ECO-305",
+              subjectName: "Economics",
+              subjectCredit: 2,
+              beforeFinalMarks: 15,
+              finalMarks: "0.00",
+              totalMarks: "Submitted",
+              letterGrade: "Submitted",
+              gpa: "Submitted"
+            },
+            {
+              subjectCode: "ICT-307",
+              subjectName: "Algorithm Design",
+              subjectCredit: 3,
+              beforeFinalMarks: 28,
+              finalMarks: "0.00",
+              totalMarks: "Submitted",
+              letterGrade: "Submitted",
+              gpa: "Submitted"
+            },
+            {
+              subjectCode: "ICT-308",
+              subjectName: "Algorithm Design Sessional",
+              subjectCredit: 1,
+              beforeFinalMarks: 23,
+              finalMarks: "0.00",
+              totalMarks: "Submitted",
+              letterGrade: "Submitted",
+              gpa: "Submitted"
+            },
+            {
+              subjectCode: "ICT-309",
+              subjectName: "Numerical Analysis",
+              subjectCredit: 3,
+              beforeFinalMarks: 23,
+              finalMarks: "0.00",
+              totalMarks: "Submitted",
+              letterGrade: "Submitted",
+              gpa: "Submitted"
+            },
+            {
+              subjectCode: "ICT-310",
+              subjectName: "Numerical Analysis Sessional",
+              subjectCredit: 1,
+              beforeFinalMarks: 23,
+              finalMarks: "0.00",
+              totalMarks: "Submitted",
+              letterGrade: "Submitted",
+              gpa: "Submitted"
+            },
+            {
+              subjectCode: "ACC-311",
+              subjectName: "Financial and Managerial Accounting",
+              subjectCredit: 2,
+              beforeFinalMarks: 15,
+              finalMarks: "0.00",
+              totalMarks: "Submitted",
+              letterGrade: "Submitted",
+              gpa: "Submitted"
+            }
+          ],
+          viva: {},
+          thesis: {},
+          project: {},
+          internship: {},
+          industryAttachment: {}
+        }
+      ]
+    }
+  }
   generatePDF(contentId: string): void {
     const element = document.getElementById(contentId);
 
@@ -60,7 +488,8 @@ export class PdfComponent {
   }
 
   generatePdf() {
-    this.pdf.gradeSheetReport();
+    this.pdf.gradeSheetReport(this.data.payload);
+    // this.pdf.test(this.data.payload);
   }
 
 
